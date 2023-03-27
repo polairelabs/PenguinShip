a0809a519b3d822d6be5212a999701c9da10515b
madgustoo <agha97a@gmail.com>
2023-03-27 01:34:41 -0400

    Quick fix

--- src/main/java/com/navaship/api/util/ProfileHelper.java
+++ src/main/java/com/navaship/api/util/ProfileHelper.java
@@ -1,9 +1,11 @@
 package com.navaship.api.util;
 
+import lombok.AllArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class ProfileHelper {
     private static final String DEV_PROFILE = "dev";
     private static final String PROD_PROFILE = "prod";
