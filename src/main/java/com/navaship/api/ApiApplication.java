bf5187fba980ebe0247fe770ddfbd852f39e6a42
madgustoo <agha97a@gmail.com>
2023-04-02 18:38:38 -0400

    Args print

--- src/main/java/com/navaship/api/ApiApplication.java
+++ src/main/java/com/navaship/api/ApiApplication.java
@@ -16,6 +16,7 @@ public class ApiApplication {
     }
 
     public static void main(String[] args) {
+        System.out.println("A.A " + "My arguments:");
         for (int i = 0; i < args.length; i++) {
             System.out.println("Argument " + (i+1) + ": " + args[i]);
         }
