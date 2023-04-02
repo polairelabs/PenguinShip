68b13c8f3aa90c3c89fa721fbbe7cf7d4b747807
madgustoo <agha97a@gmail.com>
2023-04-02 18:42:32 -0400

    Release again (#38)
    * Release
    
    * Quick test
    
    * Args print
    
    * Args print

--- src/main/java/com/navaship/api/ApiApplication.java
+++ src/main/java/com/navaship/api/ApiApplication.java
@@ -16,6 +16,10 @@ public class ApiApplication {
     }
 
     public static void main(String[] args) {
+        System.out.println("A.A " + "My arguments:");
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Argument " + (i+1) + ": " + args[i]);
+        }
         SpringApplication.run(ApiApplication.class, args);
     }
 }
