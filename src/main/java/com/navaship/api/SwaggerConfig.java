420715d8270ba8c47c499e2be080584126c0fbf0
madgustoo <agha97a@gmail.com>
2023-04-03 23:51:40 -0400

    Swagger doc for local development

--- src/main/java/com/navaship/api/SwaggerConfig.java
+++ src/main/java/com/navaship/api/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.navaship.api;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.*;
@@ -24,14 +25,16 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .paths(PathSelectors.regex("^(?!/error.*$).*"))
                 .build()
                 .apiInfo(apiInfo())
                 .securitySchemes(Collections.singletonList(apiKey()))
-                .securityContexts(Collections.singletonList(securityContext()));
+                .securityContexts(Collections.singletonList(securityContext()))
+                .ignoredParameterTypes(JwtAuthenticationToken.class);
     }
 
     private ApiKey apiKey() {
-        return new ApiKey("Access Token", "Authorization", "header");
+        return new ApiKey("Bearer", "Authorization", "header");
     }
 
     private SecurityContext securityContext() {
@@ -44,16 +47,16 @@ public class SwaggerConfig {
         AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
         AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
         authorizationScopes[0] = authorizationScope;
-        return Collections.singletonList(new SecurityReference("JWT", authorizationScopes));
+        return Collections.singletonList(new SecurityReference("Bearer", authorizationScopes));
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfo(
                 "Navaship API",
-                "Here you will find all the API endpoints, and some with examples. Don't be afraid to look at the code for more info!",
+                "Here you will find all the API endpoints with examples. To use authenticated endpoints, use the top right 'Authorize' button to add a 'Bearer' token. It must follow this format: 'Bearer your_long_jwt_access_token'. Don't be afraid to look at the code for more info!",
                 "1.0",
                 "Terms of service",
-                new Contact("Polaire labs", "https://www.polairelabs.com/", "ahmad@polairelabs.com"),
+                new Contact("Polaire labs", "https://www.polairelabs.com/", "info@polairelabs.com"),
                 "License of API",
                 "",
                 Collections.emptyList());
