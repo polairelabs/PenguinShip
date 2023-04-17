e7be8875e4448182ccf97570bc5f740736a9043d
madgustoo <agha97a@gmail.com>
2023-04-17 10:08:21 -0400

    Test

--- src/main/java/com/navaship/api/auth/AuthenticationController.java
+++ src/main/java/com/navaship/api/auth/AuthenticationController.java
@@ -283,8 +283,8 @@ public class AuthenticationController {
         // Create the server side cookie with HttpOnly set to true which contains the refresh token
         return ResponseCookie.from(REFRESH_TOKEN_COOKIE_KEY, refreshToken)
                 .maxAge(refreshTokenExpirationMs / 1000)
-                //.domain(".navaship.io")
-                .httpOnly(true)
+                .domain(".navaship.io")
+                //.httpOnly(true)
                 .sameSite("None")
                 .secure(true)
                 .path("/")
@@ -299,8 +299,8 @@ public class AuthenticationController {
                 if (cookie.getName().equals(cookieName)) {
                     ResponseCookie clearCookie = ResponseCookie.from(cookie.getName(), "")
                             .maxAge(0)
-                            //.domain(".navaship.io")
-                            .httpOnly(isHttpOnly)
+                            .domain(".navaship.io")
+                            //.httpOnly(isHttpOnly)
                             .sameSite("None")
                             .secure(true)
                             .path("/")
